614a20a084bbf3b64bbdeeb45fcd6b2b3f1e4188
Radovan Bartanus <bartanusr@gmail.com>
2020-10-05 20:05:19 +0200

    feat(sensors): sensor reading

--- sensor_check.py
+++ sensor_check.py
@@ -8,6 +8,8 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 from config import settings
+import Adafruit_DHT
+
 
 SENSOR_DATA_COLLECTION = u'sensors'
 SETTINGS_COLLECTION = u'settings'
@@ -43,26 +45,58 @@ class DbClient(object):
 
 
 class RaspiInterface(object):
+    DELAY_SECONDS = 2
+    RETRIES = 15
+
+    # TODO make generic
+    def __init__(self, raspi_client: Adafruit_DHT, sensor: int, gpio: int):
+        self.gpio = gpio
+        self.sensor = sensor
+        self.raspi_client = raspi_client
+        self.platform = self.raspi_client.get_platform()
+
     def apply_settings(self, settings: GrowBoxSettings):
         pass
 
     def get_sensor_data(self) -> Tuple[float, float]:
-        return 26, 60
+        for i in range(self.RETRIES):
+            humidity, temperature = raspi.raspi_client.read(self.sensor, self.gpio, self.platform)
+            if humidity is not None and temperature is not None:
+                return humidity, temperature
+            time.sleep(self.DELAY_SECONDS)
+        return 0.0, 0.0
+
+
+class Device(dict):
+    start_time: str
+    end_time: str
+    working: bool
+    max_temp: float
+    min_temp: float
+    max_humidity: float
+    min_humidity: float
+    max_ph: float
+    min_ph: float
+    on_period: str
+    off_period: str
 
 
 if __name__ == '__main__':
     cred = credentials.ApplicationDefault()
     project_id = settings.project_id
+    sensor_gpio = settings.gpio
+    sensor_model = settings.sensor_model
     # Use the application default credentials
     firebase_admin.initialize_app(cred, {
         'projectId': project_id,
     })
+    sensor = Adafruit_DHT.DHT11
     db = firestore.client()
     client = DbClient(db)
-    raspi = RaspiInterface()
-    while (True):
+    raspi = RaspiInterface(Adafruit_DHT, sensor_model, sensor_gpio)
+    while True:
         growbox_settings = client.read_settings()
         raspi.apply_settings(growbox_settings)
-        temp, humidity = raspi.get_sensor_data()
+        humidity, temp = raspi.get_sensor_data()
         client.put_sensor_data(datetime.datetime.now(), temp, humidity)
         time.sleep(60)
